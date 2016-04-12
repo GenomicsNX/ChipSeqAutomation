24702b79ccc8f2a022c54d175b8c316639d12923
j1angvei <mjiangwei@outlook.com>
2016-04-12 09:35:35 -0400

    add

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+	echo 'you forgot to input commit description!'
+	exit
+elif [ $# -gt 1 ]; then
+	echo 'wrap your description with "", like this,"your-description".'
+	exit
+else 
+	git add '-A'
+	git commit '-m'  $1
+	git push origin master
+fi
+
