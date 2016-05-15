23e0a957822a32040a5f14641e1abe24cd1c21c2
j1angvei <mjiangwei@outlook.com>
2016-05-15 19:46:47 +0800

    bug fix, add comment in qsub.sh

--- qsub.sh
+++ qsub.sh
@@ -8,8 +8,11 @@
 #init parameter
 work=`pwd`
 
-#import settting
+#import configuration
 source config/directory.conf	
 
-echo "sh ${work}/main.sh" | qsub -l nodes=1:ppn=4,walltime=1200:00:00 -N qsub_main -d ${work} -e ${work}/${dir_log} -o ${work}/${dir_log}
+#echo command to qsub
+echo "sh ${work}/main.sh" | qsub -l nodes=1:ppn=4,walltime=1200:00:00 -N QSUB_CHIPSEQ -d ${work} -e ${work}/${dir_log} -o ${work}/${dir_log}
+
+#echo submit successfully information
 echo -e "submit job successfully! log file located at ${work}/${dir_log}."
